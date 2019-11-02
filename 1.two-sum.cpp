0f07c400b8cc935def9465b73f977df8ebc571ae
Ctr0 <295895691@qq.com>
2019-11-02 15:10:38 -0700

    cpp solution

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -9,35 +9,35 @@
 #include <vector>
 using namespace std;
 static auto x = []() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    return 0;
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  return 0;
 }();
 
 class Solution {
-   public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> indices;
-        int lenOfVectors = nums.size();
-        bool found = false;
-        vector<pair<int, int>> new_nums;
-        for (int i = 0; i < lenOfVectors; ++i) {
-            new_nums.push_back(make_pair(nums[i], i));
-        }
-        sort(new_nums.begin(), new_nums.end());
-        int j = 0, k = lenOfVectors - 1;
-        while (j < k) {
-            if (new_nums[j].first + new_nums[k].first == target) {
-                indices.push_back(new_nums[j].second);
-                indices.push_back(new_nums[k].second);
-                break;
-            } else if (new_nums[j].first + new_nums[k].first < target) {
-                j++;
-            } else {
-                k--;
-            }
-        }
-        return indices;
+ public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> indices;
+    int lenOfVectors = nums.size();
+    bool found = false;
+    vector<pair<int, int>> new_nums;
+    for (int i = 0; i < lenOfVectors; ++i) {
+      new_nums.push_back(make_pair(nums[i], i));
     }
+    sort(new_nums.begin(), new_nums.end());
+    int j = 0, k = lenOfVectors - 1;
+    while (j < k) {
+      if (new_nums[j].first + new_nums[k].first == target) {
+        indices.push_back(new_nums[j].second);
+        indices.push_back(new_nums[k].second);
+        break;
+      } else if (new_nums[j].first + new_nums[k].first < target) {
+        j++;
+      } else {
+        k--;
+      }
+    }
+    return indices;
+  }
 };
 // @lc code=end
