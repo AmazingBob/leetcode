e86229cd172d398d59494f8fb4a9ab3481131f42
Ctr0 <amazingbcj@gmail.com>
2020-05-25 23:38:20 -0700

    update

--- 441.arranging-coins.java
+++ 441.arranging-coins.java
@@ -24,11 +24,11 @@ class Solution {
 // @lc code=end
 
 
-// class Test {
-//   public static void main(String[] args) {
-//     int n = 5;
-//     Solution sltn = new Solution();
-//     int res = sltn.arrangeCoins(n);
-//     return;
-//   }
-// }
+class Test {
+  public static void main(String[] args) {
+    int n = 5;
+    Solution sltn = new Solution();
+    int res = sltn.arrangeCoins(n);
+    return;
+  }
+}
