b422f8c4d3aa0b01682e415d652f293dc5deb22e
Ctr0 <amazingbcj@gmail.com>
2020-06-06 13:34:50 -0700

    update

--- 704.binary-search.java
+++ 704.binary-search.java
@@ -9,9 +9,12 @@ class Solution {
   public int search(int[] nums, int target) {
     int lo = 0, hi = nums.length - 1, mid = (lo + hi) / 2;
     while (lo <= hi) {
-      if (nums[mid] == target) return mid;
-      if (nums[mid] < target) lo = mid + 1;
-      if (nums[mid] > target) hi = mid - 1;
+      if (nums[mid] == target)
+        return mid;
+      if (nums[mid] < target)
+        lo = mid + 1;
+      if (nums[mid] > target)
+        hi = mid - 1;
       mid = (lo + hi) / 2;
     }
     return -1;
