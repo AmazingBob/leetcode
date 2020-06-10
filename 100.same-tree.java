9bc8830fe17f027da39897bd8922cb38b35f58b4
Ctr0 <amazingbcj@gmail.com>
2020-06-09 21:51:29 -0700

    Update

--- 100.same-tree.java
+++ 100.same-tree.java
@@ -6,24 +6,24 @@
 
 // @lc code=start
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
  */
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if(p == null && q == null) return true;
+    if(p != null && q != null){
+      if(p.val == q.val){
+        if(!isSameTree(p.left, q.left)) return false;
+        if(!isSameTree(p.right, q.right)) return false;
+        return true;
+      } else {
+        return false;
+      }
     }
+    return false;
+  }
 }
 // @lc code=end
-
