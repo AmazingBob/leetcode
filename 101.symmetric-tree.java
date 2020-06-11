7ba697408a0535a429ba8512dbe0b320ff70aac7
Ctr0 <amazingbcj@gmail.com>
2020-06-10 20:37:26 -0700

    Update

--- 101.symmetric-tree.java
+++ 101.symmetric-tree.java
@@ -6,24 +6,21 @@
 
 // @lc code=start
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode
+ * right; TreeNode() {} TreeNode(int val) { this.val = val; } TreeNode(int val, TreeNode left,
+ * TreeNode right) { this.val = val; this.left = left; this.right = right; } }
  */
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        
-    }
+  public boolean isSymmetric(TreeNode root) {
+    if (root == null) return true;
+    return dfs(root.left, root.right);
+  }
+
+  private static boolean dfs(TreeNode left, TreeNode right) {
+    if (left == null && right == null) return true;
+    else if (left == null || right == null) return false;
+    else if (left.val != right.val) return false;
+    return dfs(left.left, right.right) && dfs(left.right, right.left);
+  }
 }
 // @lc code=end
-
