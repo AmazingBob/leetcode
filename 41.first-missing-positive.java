b48abd02e16a8f0af54469b8312918e02be44aa7
Ctr0 <amazingbcj@gmail.com>
2020-06-15 16:35:27 -0700

    Update

---
+++ 41.first-missing-positive.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=41 lang=java
+ *
+ * [41] First Missing Positive
+ */
+
+// @lc code=start
+class Solution {
+  public int firstMissingPositive(int[] nums) {
+    // Corner case
+    if (nums == null || nums.length == 0) return 1;
+    // Sorting
+    for (int i = 0; i < nums.length; i++) {
+      while (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
+        int temp = nums[nums[i] - 1];
+        nums[nums[i] - 1] = nums[i];
+        nums[i] = temp;
+      }
+    }
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != i + 1) return i + 1;
+    }
+    return nums.length + 1;
+  }
+}
+// @lc code=end
