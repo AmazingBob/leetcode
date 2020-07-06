f62dd4e20389b5f49e35e60b9b8f620de66492e6
Ctr0 <amazingbcj@gmail.com>
2020-07-06 16:25:03 -0700

    Update

---
+++ 63.unique-paths-ii.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=63 lang=java
+ *
+ * [63] Unique Paths II
+ */
+
+// @lc code=start
+class Solution {
+  public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    // Corner case
+    if (obstacleGrid == null || obstacleGrid.length == 0) return 0;
+    // DP
+    // Time: O(m * n)
+    // Space: O(m * n)
+    int row = obstacleGrid.length;
+    int col = obstacleGrid[0].length;
+    if (obstacleGrid[0][0] == 1) return 0;
+    // Initiating
+    obstacleGrid[0][0] = 1;
+    for (int i = 1; i < col; i++) {
+      obstacleGrid[0][i] = (obstacleGrid[0][i] == 0 && obstacleGrid[0][i - 1] == 1) ? 1 : 0;
+    }
+    for (int i = 1; i < row; i++) {
+      obstacleGrid[i][0] = (obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1) ? 1 : 0;
+    }
+    // DP
+    // Time: O(m * n)
+    // Space: O(1)
+    for (int i = 1; i < row; i++) {
+      for (int j = 1; j < col; j++) {
+        if (obstacleGrid[i][j] == 0) {
+          obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+        } else {
+          obstacleGrid[i][j] = 0;
+        }
+      }
+    }
+    return obstacleGrid[row - 1][col - 1];
+  }
+}
+// @lc code=end
