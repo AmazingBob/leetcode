9f92677032243ca4833cb12969f86a32d2ec3796
Ctr0 <amazingbcj@gmail.com>
2020-07-09 13:47:54 -0700

    Update

---
+++ 64.minimum-path-sum.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=64 lang=java
+ *
+ * [64] Minimum Path Sum
+ */
+
+// @lc code=start
+class Solution {
+  public int minPathSum(int[][] grid) {
+    // Corner case
+    if (grid == null || grid.length == 0) return 0;
+    // Initiating
+    int row = grid.length;
+    int col = grid[0].length;
+    for (int i = 1; i < col; i++) {
+      grid[0][i] += grid[0][i - 1];
+    }
+    for (int i = 1; i < row; i++) {
+      grid[i][0] += grid[i - 1][0];
+    }
+    // DP
+    // Time: O(m * n)
+    // Space: O(1)
+    for (int i = 1; i < row; i++) {
+      for (int j = 1; j < col; j++) {
+        grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+      }
+    }
+    return grid[row - 1][col - 1];
+  }
+}
+// @lc code=end
