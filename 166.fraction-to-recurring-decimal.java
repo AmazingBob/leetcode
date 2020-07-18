4e6888046579d60236a5f2be42584bea8a637910
Ctr0 <amazingbcj@gmail.com>
2020-07-17 17:39:40 -0700

    Update

---
+++ 166.fraction-to-recurring-decimal.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+
+/*
+ * @lc app=leetcode id=166 lang=java
+ *
+ * [166] Fraction to Recurring Decimal
+ */
+
+// @lc code=start
+class Solution {
+  public String fractionToDecimal(int numerator, int denominator) {
+    // Corner case
+    if (numerator == 0) return "0";
+
+    StringBuilder sb = new StringBuilder();
+    if (numerator > 0 && denominator < 0 || numerator < 0 && denominator > 0) sb.append("-");
+
+    long divisor = Math.abs((long) numerator);
+    long dividend = Math.abs((long) denominator);
+    long remainder = divisor % dividend;
+    sb.append(divisor / dividend);
+    if (remainder == 0) {
+      return sb.toString();
+    }
+    sb.append(".");
+    HashMap<Long, Integer> map = new HashMap<>();
+    while (remainder != 0) {
+      if (map.containsKey(remainder)){
+        sb.insert(map.get(remainder), "(");
+        sb.append(")");
+        break;
+      }
+      map.put(remainder, sb.length());
+      remainder *= 10;
+      sb.append(remainder / dividend);
+      remainder %= dividend;
+    }
+    
+    return sb.toString();
+  }
+}
+// @lc code=end
