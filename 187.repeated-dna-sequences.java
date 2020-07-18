8fdb38615adfcfc0ebef8e764a0d8d383da40fb2
Ctr0 <amazingbcj@gmail.com>
2020-07-17 17:40:15 -0700

    Update

---
+++ 187.repeated-dna-sequences.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/*
+ * @lc app=leetcode id=187 lang=java
+ *
+ * [187] Repeated DNA Sequences
+ */
+
+// @lc code=start
+class Solution {
+  public List<String> findRepeatedDnaSequences(String s) {
+    HashSet<String> seen = new HashSet<String>();
+    HashSet<String> repeated = new HashSet<String>();
+
+    for (int i = 0; i < s.length() - 9; i++) {
+      String temp = s.substring(i, i + 10);
+      if (!seen.add(temp)){
+        repeated.add(temp);
+      }
+    }
+
+    return new ArrayList<String>(repeated);
+  }
+}
+// @lc code=end
